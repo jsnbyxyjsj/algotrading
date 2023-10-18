6abf0f5df56758eef49202b15762e3cfa9c7375b
biagio <paparell@ins.uni-bonn.de>
2023-10-18 06:21:07 -0400

    Backtrack strategy is essentially ready!

--- sig_prediction.py
+++ sig_prediction.py
@@ -11,40 +11,8 @@ from libqoi import mix_and_split
 import signatory as sg
 
 
-### THIS FUNCTION MUST BE FINISHED
-def backtracking(x_true, y_pred, error, stock_units):
-    x = x_true.reshape(-1)
-    y = y_pred.reshape(-1)
-    n_times = x.shape[0]
-    wallet = 0.
-    status = ""
-    action = ""
-    for nth in range(n_times):
-        predicted_val = y[nth]
-        curr_val = x[nth]
-        future_val = x[nth+1]
-        percentage = predicted_val / 100. * error
-        up_val = predicted_val + percentage
-        low_val = predicted_val - percentage
-
-        status = "TOO UNCERTAIN"
-        action="IDLE"
-        if low_val > x_tmp[-1]:
-            status = "INCREASES"
-            mycolor="green"
-            action="BUY"
-        elif up_val < x_tmp[-1]:
-            status = "DECREASES"
-            mycolor="red"
-            action="SELL"
-        print(f"{action}: price {status}: {x_tmp[-1]:.2f} -> {predicted_val:.2}")
-
-    return wallet
-#---
-
-
 n_times = 2000
-window = 10
+window = 20
 sig_depth = 5
 
 
@@ -61,7 +29,7 @@ x_tmp = torch.abs(torch.normal(0., 1., (n_times,)))
 db0 = single_ts_to_windowed(x_tmp, window)
 
 # removing the last "window chunk", since we do not know its label.
-# (label = immediate next price, and we don't know the future)
+# (label = next price, and we don't know the future)
 n_samples = db0.shape[0] - 1
 db0_without_last = db0[:n_samples]
 assert (db0_without_last.shape[0] == (n_times - window))
@@ -93,13 +61,9 @@ y_initial_pred = model(x_data).detach()
 # Error estimation
 start_mre = relerr(y_data, y_initial_pred, 1e-2)
 print(f"Starting mean Relative Error: {start_mre:.1}%")
-
-
+# Set Optimizer and loss function
 optimizer = optim.Adam(model.parameters(), lr=1e-5)
-
-# And the loss functions, canonical
 loss_fn = nn.MSELoss()
-
 n_epochs = 30000
 # History of loss function values
 hist_loss_train = torch.ones(n_epochs)
@@ -115,8 +79,9 @@ for nth in range(n_epochs):
     with torch.no_grad():
         y_val_pred = model(x_val)
         loss_val = loss_fn(y_val_pred, y_val)
-        print(f"{nth+1}/{n_epochs} t: {loss_train.item():.3e} ", end=' ')
-        print(f"v:{loss_val.item():.3e}")
+        if n_epochs % 1000 == 0:
+            print(f"{nth+1}/{n_epochs} t: {loss_train.item():.3e} ", end=' ')
+            print(f"v:{loss_val.item():.3e}")
         hist_loss_train[nth] = loss_train.item()
         hist_loss_val[nth] = loss_val.item()
 
@@ -133,7 +98,7 @@ plt.title("Loss function evolution")
 plt.show()
 
 #
-# (4) EVALUATE THE RESULTS
+# (4) EVALUATE THE RESULTS BY COMPUTING THE MEAN RELATIVE AND SQUARE ERROR
 #
 # Store the overall predicted results
 y_pred = model(x_data).detach()
@@ -160,6 +125,58 @@ plt.grid()
 plt.show()
 
 
+#
+# (5) VERIFY THE TRADING STRATEGY VIA BACKTRACKING
+#
+
+# "ratio" represents how many stock are going to buy and sell each time
+ratio = 1.0
+transaction_cost = 0.1
+
+wallet = 0.
+for nth in range(y_data.shape[0]):
+    # Determine the current value, the true next one and the predicted
+    x_next = y_data[nth].item()
+    x_pred = y_pred[nth].item()
+    t = nth + window - 1
+    x_now = x_tmp[t]
+    print(f"time {t}. Curr: {x_now:.2f} Pred: {x_pred:.2f} True: {x_next:.2f}")
+    # Determine if buy, sell or stay idle
+    percentage = x_pred / 100. * mre
+    up_val = x_pred + percentage
+    low_val = x_pred - percentage
+    profit = 0
+    status = "TOO UNCERTAIN"
+    mycolor="grey"
+    action="IDLE"
+    # If the lowest predicted value is higher than the current,
+    # even considering the transaction costs...
+    if low_val - 2*transaction_cost - x_now > 0:
+        #...buy a stock percentage, sell the instant later
+        status = "INCREASES"
+        mycolor="green"
+        action="BUY"
+        profit = x_next - x_now - 2*transaction_cost
+    # If the highest predicted value is lower than the current,
+    # even considering the transaction costs...
+    elif x_now - up_val - 2*transaction_cost > 0:
+        # ...sell a stock percentage
+        status = "DECREASES"
+        mycolor="red"
+        action="SELL"
+        profit = x_now - 2*transaction_cost - x_next
+    wallet += profit
+    print(f" --- action: {action}. Curr wallet: {wallet:.3f}")
+   
+
+print(f"Final wallet: {wallet:.3f}")
+print(f"(--- (considering transaction costs of {transaction_cost:.3f} ---)")
+#input("OK?")
+
+#
+# DETERMINE WHICH NEXT ACTION TO TAKE
+#
+
 tmp_to_predict = augment_db(to_log_returns(db0[-1].reshape(1, -1)))
 next_to_predict = sg.signature(tmp_to_predict, depth = sig_depth)
 predicted_val = model(next_to_predict).detach().item()
@@ -171,24 +188,22 @@ print(f"PREDICTED NEXT VALUE: {predicted_val:.2f} [{low_val}, {up_val}]")
 status = "TOO UNCERTAIN"
 mycolor="grey"
 action="IDLE"
-if low_val > x_tmp[-1]:
+expected_profit = 0.
+if low_val - 2*transaction_cost > x_tmp[-1]:
     status = "INCREASES"
     mycolor="green"
     action="BUY"
-elif up_val < x_tmp[-1]:
+    expected_profit = low_val - 2*transaction_cost - x_tmp[-1]
+elif up_val + 2*transaction_cost < x_tmp[-1]:
     status = "DECREASES"
     mycolor="red"
     action="SELL"
+    expected_profit = - up_val - 2*transaction_cost + x_tmp[-1]
 print(f"{action}: price {status}: {x_tmp[-1]:.2f} -> {predicted_val:.2}")
-
-
-# Perform BACKTRACKING
-n_stocks = 1.
-money = backtracking(x_tmp[window-1:-1], y_pred, mre, n_stocks)
-print(f"Backtracked strategy on {n_stocks} stocks: {money:.3f}")
+print(f"Expected profit: {expected_profit:.3f}")
 
 x_mean = x_tmp.mean()
-time_to_show = 50
+time_to_show = 10
 plt.plot(range(time_to_show), x_tmp[-time_to_show:], color='orange')
 plt.plot(time_to_show, predicted_val, color=mycolor, marker="X")
 plt.plot(time_to_show, up_val, color=mycolor, marker="v")
